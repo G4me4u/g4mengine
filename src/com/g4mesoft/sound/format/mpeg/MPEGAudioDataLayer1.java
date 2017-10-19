0bdae08d4826f0b5bcfccbbe4a9e8d8484186162
G4me4u <G4me4u@gmail.com>
2017-10-20 00:47:59 +0200

    Optimized layer 1 audio decoding with lookup and fewer for-loops

--- src/com/g4mesoft/sound/format/mpeg/MPEGAudioDataLayer1.java
+++ src/com/g4mesoft/sound/format/mpeg/MPEGAudioDataLayer1.java
@@ -18,61 +18,75 @@ public class MPEGAudioDataLayer1 {
 		samples = new float[2 * SAMPLES_PER_SB_CH * SUBBANDS_PER_CH];
 	}
 	
-	public boolean loadAudioData(MP3BitStream bitStream, MPEGFrame frame, MPEGSynthesisSubbandFilter synthesisFilter) throws IOException {
-		int bound = (frame.header.mode == MPEGHeader.JOINT_STEREO) ? MPEGTables.BOUND_TABLE[frame.header.mode_extension] : SUBBANDS_PER_CH;
+	public boolean readAudioData(MP3BitStream bitStream, MPEGFrame frame, MPEGSynthesisSubbandFilter synthesisFilter) throws IOException {
+		int bound = (frame.header.mode == MPEGHeader.JOINT_STEREO) ? 
+				MPEGTables.BOUND_TABLE[frame.header.mode_extension] : 
+				SUBBANDS_PER_CH;
 		
-		int sb, ch, nb;
+		int sb, ch;
+
+		// Read allocation data
 		for (sb = 0; sb < bound; sb++)
 			for (ch = 0; ch < frame.nch; ch++)
 				if ((allocation[ch][sb] = bitStream.readBits(4)) == 0xF)
 					return false;
 		for ( ; sb < SUBBANDS_PER_CH; sb++)
-			if ((allocation[0][sb] = allocation[1][sb] = bitStream.readBits(4)) == 0xF)
-				return false;
+			if ((allocation[0][sb] = bitStream.readBits(4)) == 0xF)
+				return false; // allocation[1][sb] is not used here.
+		
+		// Read scalefactors
 		for (sb = 0; sb < SUBBANDS_PER_CH; sb++)
 			for (ch = 0; ch < frame.nch; ch++)
 				if (allocation[ch][sb] != 0)
 					if ((scalefactor[ch][sb] = bitStream.readBits(6)) == 0x3F)
 						return false;
 		
-		if (bitStream.isEndOfStream())
-			return false;
-		
 		int sp = 0;
 		int pa = frame.nch == 1 ? 2 : 1;
 
+		int alloc;
+		float sample;
+		
+		// Read samples
 		for (int s = 0; s < SAMPLES_PER_SB_CH; s++) {
 			for (sb = 0; sb < bound; sb++)
 				for (ch = 0; ch < frame.nch; ch++) {
-					nb = MPEGTables.L1_BITS_PER_SAMPLE_TABLE[allocation[ch][sb]];
-					samples[sp] = (nb != 0) ? requantize(bitStream, nb) : 0.0f;
+					if ((alloc = allocation[ch][sb]) != 0) {
+						sample = requantize(bitStream, MPEGTables.L1_BITS_PER_SAMPLE_TABLE[alloc]);
+						
+						// Apply scalefactors
+						samples[sp] = sample * MPEGTables.L12_SCALEFACTOR_TABLE[scalefactor[ch][sb]];
+					} else samples[sp] = 0.0f;
 					
 					sp += pa;
 				}
 			for ( ; sb < SUBBANDS_PER_CH; sb++) {
-				nb = MPEGTables.L1_BITS_PER_SAMPLE_TABLE[allocation[0][sb]];
-				samples[sp++] = samples[sp++] = (nb != 0) ? requantize(bitStream, nb) : 0.0f;
+				if ((alloc = allocation[0][sb]) != 0) {
+					sample = requantize(bitStream, MPEGTables.L1_BITS_PER_SAMPLE_TABLE[alloc]);
+
+					// Apply scalefactors
+					samples[sp++] = sample * MPEGTables.L12_SCALEFACTOR_TABLE[scalefactor[0][sb]];
+					samples[sp++] = sample * MPEGTables.L12_SCALEFACTOR_TABLE[scalefactor[1][sb]];
+				} else samples[sp++] = samples[sp++] = 0.0f;
 			}
 		}
 		
 		if (bitStream.isEndOfStream())
 			return false;
 		
-		// Rescale samples
-		rescale(frame.nch);
 		// Synthesis subband filter
 		synthesisFilter.synthesizeSamples(samples, frame.nch, SAMPLES_PER_SB_CH);
 		
 		// If single channel, copy the
 		// samples to the right channel.
 		if (frame.nch == 1) {
-			float sample;
-			int i = 0;
-			for (int s = 0; s < SAMPLES_PER_SB_CH; s++)
+			sp = 0;
+			for (int s = 0; s < SAMPLES_PER_SB_CH; s++) {
 				for (sb = 0; sb < SUBBANDS_PER_CH; sb++) {
-					sample = samples[i++];
-					samples[i++] = sample;
+					sample = samples[sp++];
+					samples[sp++] = sample;
 				}
+			}
 		}
 		
 		return true;
@@ -82,34 +96,17 @@ public class MPEGAudioDataLayer1 {
 		int sample = bitStream.readBits(nb);
 		int mask = MPEGTables.SIGN_MASK_TABLE[nb];
 		
+		// Turn the sample into a fraction
 		float fraction = (sample & mask) != 0 ?
 			(float)(sample ^ mask) / mask :
 			(float)(sample) / mask - 1.0f;
-
-		fraction += MPEGTables.L1_QUANTIZATION_D_TABLE[nb];
-		fraction *= MPEGTables.L1_QUANTIZATION_C_TABLE[nb];
 		
-		return fraction;
+		// Requantize values using formula:
+		// s'' = (d + s''') * c
+		fraction += MPEGTables.L1_QUANTIZATION_D_TABLE[nb];
+		return fraction * MPEGTables.L1_QUANTIZATION_C_TABLE[nb];
 	}
 	
-	private void rescale(int nch) {
-		int ch, s, sp;
-		float sf;
-		
-		for (int sb = SUBBANDS_PER_CH - 1; sb >= 0; sb--) {
-			for (ch = 0; ch < nch; ch++) {
-				if (allocation[ch][sb] != 0) {
-					sf = MPEGTables.L12_SCALEFACTOR_TABLE[scalefactor[ch][sb]];
-					sp = ch + (sb << 1);
-					for (s = SAMPLES_PER_SB_CH - 1; s >= 0; s--) {
-						samples[sp] *= sf;
-						sp += SUBBANDS_PER_CH * 2;
-					}
-				}
-			}
-		}
-	}
-
 	public float[] getSamples() {
 		return Arrays.copyOf(samples, 2 * SUBBANDS_PER_CH * SAMPLES_PER_SB_CH);
 	}
