6bab4a368faee0f06dfb75308cf3d66ce964901d
G4me4u <G4me4u@gmail.com>
2017-11-20 23:21:35 +0100

    Table lookup now uses static import (layer 1)

--- src/com/g4mesoft/sound/format/mpeg/MPEGAudioDataLayer1.java
+++ src/com/g4mesoft/sound/format/mpeg/MPEGAudioDataLayer1.java
@@ -3,6 +3,13 @@ package com.g4mesoft.sound.format.mpeg;
 import java.io.IOException;
 import java.util.Arrays;
 
+import static com.g4mesoft.sound.format.mpeg.MPEGTables.BOUND_TABLE;
+import static com.g4mesoft.sound.format.mpeg.MPEGTables.L1_BITS_PER_SAMPLE_TABLE;
+import static com.g4mesoft.sound.format.mpeg.MPEGTables.L12_SCALEFACTOR_TABLE;
+import static com.g4mesoft.sound.format.mpeg.MPEGTables.SIGN_MASK_TABLE;
+import static com.g4mesoft.sound.format.mpeg.MPEGTables.L1_QUANTIZATION_D_TABLE;
+import static com.g4mesoft.sound.format.mpeg.MPEGTables.L1_QUANTIZATION_C_TABLE;
+
 public class MPEGAudioDataLayer1 {
 
 	private static final int SUBBANDS_PER_CH = 32;
@@ -20,7 +27,7 @@ public class MPEGAudioDataLayer1 {
 	
 	public boolean readAudioData(MP3BitStream bitStream, MPEGFrame frame, MPEGSynthesisSubbandFilter synthesisFilter) throws IOException {
 		int bound = (frame.header.mode == MPEGHeader.JOINT_STEREO) ? 
-				MPEGTables.BOUND_TABLE[frame.header.mode_extension] : 
+				BOUND_TABLE[frame.header.mode_extension] : 
 				SUBBANDS_PER_CH;
 		
 		int sb, ch;
@@ -55,21 +62,21 @@ public class MPEGAudioDataLayer1 {
 			for (sb = 0; sb < bound; sb++)
 				for (ch = 0; ch < frame.nch; ch++) {
 					if ((alloc = allocation[ch][sb]) != 0) {
-						sample = requantize(bitStream, MPEGTables.L1_BITS_PER_SAMPLE_TABLE[alloc]);
+						sample = requantize(bitStream, L1_BITS_PER_SAMPLE_TABLE[alloc]);
 						
 						// Apply scalefactors
-						samples[sp] = sample * MPEGTables.L12_SCALEFACTOR_TABLE[scalefactor[ch][sb]];
+						samples[sp] = sample * L12_SCALEFACTOR_TABLE[scalefactor[ch][sb]];
 					} else samples[sp] = 0.0f;
 					
 					sp += pa;
 				}
 			for ( ; sb < SUBBANDS_PER_CH; sb++) {
 				if ((alloc = allocation[0][sb]) != 0) {
-					sample = requantize(bitStream, MPEGTables.L1_BITS_PER_SAMPLE_TABLE[alloc]);
+					sample = requantize(bitStream, L1_BITS_PER_SAMPLE_TABLE[alloc]);
 
 					// Apply scalefactors
-					samples[sp++] = sample * MPEGTables.L12_SCALEFACTOR_TABLE[scalefactor[0][sb]];
-					samples[sp++] = sample * MPEGTables.L12_SCALEFACTOR_TABLE[scalefactor[1][sb]];
+					samples[sp++] = sample * L12_SCALEFACTOR_TABLE[scalefactor[0][sb]];
+					samples[sp++] = sample * L12_SCALEFACTOR_TABLE[scalefactor[1][sb]];
 				} else samples[sp++] = samples[sp++] = 0.0f;
 			}
 		}
@@ -97,7 +104,7 @@ public class MPEGAudioDataLayer1 {
 
 	private float requantize(MP3BitStream bitStream, int nb) throws IOException {
 		int sample = bitStream.readBits(nb);
-		int mask = MPEGTables.SIGN_MASK_TABLE[nb];
+		int mask = SIGN_MASK_TABLE[nb];
 		
 		// Turn the sample into a fraction
 		float fraction = (sample & mask) != 0 ?
@@ -106,8 +113,8 @@ public class MPEGAudioDataLayer1 {
 		
 		// Requantize values using formula:
 		// s'' = (d + s''') * c
-		fraction += MPEGTables.L1_QUANTIZATION_D_TABLE[nb];
-		return fraction * MPEGTables.L1_QUANTIZATION_C_TABLE[nb];
+		fraction += L1_QUANTIZATION_D_TABLE[nb];
+		return fraction * L1_QUANTIZATION_C_TABLE[nb];
 	}
 	
 	public float[] getSamples() {
