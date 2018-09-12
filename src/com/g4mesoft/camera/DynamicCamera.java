0efe077220d4819888f337cf26d44a9f86c11068
G4me4u <G4me4u@gmail.com>
2018-09-12 11:15:15 +0200

    Improved DynamicCamera functionality. Added CameraPose animations. Removed Camera shaking.

--- src/com/g4mesoft/camera/DynamicCamera.java
+++ src/com/g4mesoft/camera/DynamicCamera.java
@@ -1,418 +1,457 @@
 package com.g4mesoft.camera;
 
-import java.util.Random;
-
 import com.g4mesoft.math.Vec2f;
 
 public class DynamicCamera implements ICamera {
 
-	private static final Random random = new Random();
+	private CameraPose pose;
 
-	private float xoffs;
-	private float yoffs;
-	private float staticX;
-	private float staticY;
-	private float shakeX;
-	private float shakeY;
+	private float xMove;
+	private float yMove;
+	private float deltaZoom;
 	
-	private float xmove;
-	private float ymove;
-	
-	private float width;
-	private float height;
 	private float viewWidth;
 	private float viewHeight;
 	
-	private float bx0;
-	private float by0;
-	private float bx1;
-	private float by1;
-
-	private int shakeAmount;
-
-	private float zoomChange;
-	private float zoom;
 	private float minZoom;
 	private float maxZoom;
+
 	private boolean zoomToCenter;
-
+	
+	private CameraPose smoothingPose;
+	private float poseSmoothingFactor;
+	private float poseSmoothingTimer;
+	
 	public DynamicCamera() {
-		xoffs = 0.0f;
-		yoffs = 0.0f;
-		staticX = 0.0f;
-		staticY = 0.0f;
-
-		width = 0.0f;
-		height = 0.0f;
+		pose = new CameraPose();
+		
+		xMove = 0.0f;
+		yMove = 0.0f;
+		deltaZoom = 0.0f;
+		
 		viewWidth = 0.0f;
 		viewHeight = 0.0f;
-		
-		bx0 = Float.NEGATIVE_INFINITY;
-		by0 = Float.NEGATIVE_INFINITY;
-		bx1 = Float.POSITIVE_INFINITY;
-		by1 = Float.POSITIVE_INFINITY;
 
 		minZoom = Float.MIN_VALUE;
 		maxZoom = Float.MAX_VALUE;
-		zoom = 100.0f;
+	
+		zoomToCenter = false;
 	}
-
+	
 	public void update() {
-		finishMovement();
-		finishZoom();
+		pose.xOffset += xMove;
+		pose.yOffset += yMove;
+		xMove = 0.0f;
+		yMove = 0.0f;
 		
-		if (shakeAmount > 0) {
-			shakeAmount--;
+		pose.zoom += deltaZoom;
+		deltaZoom = 0.0f;
+	
+		animatePose();
+	}
+	
+	protected void animatePose() {
+		// If we're done animating the
+		// smoothing pose is null.
+		if (smoothingPose == null)
+			return;
+
+		// No reason to animate, if there's
+		// nothing to change.
+		if (!smoothingPose.offsetSet && !smoothingPose.zoomSet) {
+			smoothingPose = null;
+			return;
+		}
+		
+		float oldSmoothingTimer = poseSmoothingTimer;
+		poseSmoothingTimer += poseSmoothingFactor;
+		
+		// We might change the smoothingPose
+		// field. Make a copy to be sure we
+		// don't get a NullPointerException
+		// when the animation finishes.
+		CameraPose sPose = smoothingPose;
+
+		float c;
+		if (poseSmoothingTimer >= 1.0f) {
+			c = 1.0f;
 			
-			shakeX = random.nextFloat() - 0.5f;
-			shakeY = random.nextFloat() - 0.5f;
+			// If we're ever at this point,
+			// the animation has completed.
+			smoothingPose = null;
 		} else {
-			shakeX = 0.0f;
-			shakeY = 0.0f;
+			c = poseSmoothingFactor / (1.0f - oldSmoothingTimer);
 		}
-	}
-
-	private void finishMovement() {
-		xoffs += xmove;
-		yoffs += ymove;
-		xmove = 0.0f;
-		ymove = 0.0f;
 		
-		if (Float.isNaN(xoffs))
-			xoffs = bx0;
-		if (Float.isNaN(yoffs))
-			yoffs = by0;
-	}
-	
-	private void fixBounds() {
-		if (xoffs + xmove < bx0) {
-			xmove = bx0 - xoffs;
-		} else if (xoffs + xmove + viewWidth > bx1) {
-			xmove = bx1 - xoffs - viewWidth;
+		if (sPose.offsetSet) {
+			float dx = sPose.xOffset - pose.xOffset;
+			float dy = sPose.yOffset - pose.yOffset;
+
+			moveXOffset(dx * c);
+			moveYOffset(dy * c);
 		}
 
-		if (yoffs + ymove < by0) {
-			ymove = by0 - yoffs;
-		} else if (yoffs + ymove + viewHeight > by1) {
-			ymove = by1 - yoffs - viewHeight;
-		}
+		if (sPose.zoomSet) {
+			float dz = sPose.zoom - pose.zoom;
 
-		if (Float.isNaN(xmove))
-			xmove = 0.0f;
-		if (Float.isNaN(ymove))
-			ymove = 0.0f;
+			addZoom(dz * c);
+		}
 	}
 	
-	public void move(float xm, float ym) {
-		xmove += xm;
-		ymove += ym;
-
-		fixBounds();
+	public void setXOffset(float xOffset) {
+		moveXOffset(xOffset - pose.xOffset);
 	}
 
-	public void moveTo(float x, float y) {
-		move(x - xoffs - xmove - staticX, y - yoffs - ymove - staticY);
+	public void setCenterX(float xCenter) {
+		setXOffset(xCenter - viewWidth * 0.5f);
 	}
 
-	public void moveToCenter(float xc, float yc) {
-		moveTo(xc - viewWidth / 2f, yc - viewHeight / 2f);
+	public void setYOffset(float yOffset) {
+		moveYOffset(yOffset - pose.yOffset);
+	}
+	
+	public void setCenterY(float yCenter) {
+		setYOffset(yCenter - viewHeight * 0.5f);
 	}
 
-	public void setOffset(float xo, float yo) {
-		xmove = 0.0f;
-		ymove = 0.0f;
+	public void setOffset(float xOffset, float yOffset) {
+		moveOffset(xOffset - pose.xOffset, yOffset - pose.yOffset);
+	}
+	
+	public void setCenter(float xCenter, float yCenter) {
+		setOffset(xCenter - viewWidth * 0.5f, yCenter - viewHeight * 0.5f);
+	}
+
+	public void moveXOffset(float xMove) {
+		// The view has already been
+		// centered around the center
+		// of the bounding box.
+		if (viewWidth > pose.bx1 - pose.bx0)
+			return;
 		
-		xo -= staticX;
-		yo -= staticY;
-
-		if (xo < bx0) {
-			xoffs = bx0;
-		} else if (xo + viewWidth > bx1) {
-			xoffs = bx1 - viewWidth;
-		} else {
-			xoffs = xo;
-		}
-
-		if (yo < by0) {
-			yoffs = by0;
-		} else if (yo + viewHeight > by1) {
-			yoffs = by1 - viewHeight;
-		} else {
-			yoffs = yo;
-		}
-	}
-	
-	public void setOffsetCenter(float xc, float yc) {
-		setOffset(xc - viewWidth / 2f, yc - viewHeight / 2f);
-	}
-
-	public float getXOffset(boolean shake) {
-		return getXOffset(1.0f, shake);
-	}
-
-	public float getYOffset(boolean shake) {
-		return getYOffset(1.0f, shake);
-	}
-	
-	public float getXOffset(float dt, boolean shake) {
-		if (shake)
-			return xoffs + xmove * dt + staticX + shakeX;
-
-		return xoffs + xmove * dt + staticX;
-	}
-
-	public float getYOffset(float dt, boolean shake) {
-		if (shake)
-			return yoffs + ymove * dt + staticY + shakeY;
+		this.xMove += xMove;
 		
-		return yoffs + ymove * dt + staticY;
+		checkBoundsX();
 	}
 	
-	public float getXOffset() {
-		return getXOffset(true);
-	}
-
-	public float getYOffset() {
-		return getYOffset(true);
-	}
-	
-	public float getXOffset(float dt) {
-		return getXOffset(dt, true);
-	}
-
-	public float getYOffset(float dt) {
-		return getYOffset(dt, true);
-	}
-
-	public Vec2f getOffset(float dt, boolean shake) {
-		return new Vec2f(getXOffset(dt, shake), getYOffset(dt, shake));
-	}
-	
-	public Vec2f getOffset(float dt) {
-		return getOffset(dt, true);
-	}
-	
-	public Vec2f getOffset(boolean shake) {
-		return getOffset(1.0f, shake);
-	}
-	
-	public Vec2f getOffset() {
-		return getOffset(1.0f, true);
-	}
-	
-	public float getCenterX(boolean shake) {
-		return getCenterX(1.0f, shake);
-	}
-
-	public float getCenterY(boolean shake) {
-		return getCenterY(1.0f, shake);
-	}
-	
-	public float getCenterX(float dt, boolean shake) {
-		return getXOffset(dt, shake) + viewWidth / 2.0f;
-	}
-
-	public float getCenterY(float dt, boolean shake) {
-		return getYOffset(dt, shake) + viewHeight / 2.0f;
-	}
-
-	public float getCenterX() {
-		return getCenterX(true);
-	}
-
-	public float getCenterY() {
-		return getCenterY(true);
-	}
-	
-	public float getCenterX(float dt) {
-		return getCenterX(dt, true);
-	}
-
-	public float getCenterY(float dt) {
-		return getCenterX(dt, true);
-	}
-	
-	public Vec2f getCenter(float dt, boolean shake) {
-		return new Vec2f(getCenterX(dt, shake), getCenterY(dt, shake));
-	}
-	
-	public Vec2f getCenter(float dt) {
-		return getCenter(dt, true);
-	}
-	
-	public Vec2f getCenter(boolean shake) {
-		return getCenter(1.0f, shake);
-	}
-	
-	public Vec2f getCenter() {
-		return getCenter(true);
-	}
-	
-	public void setStaticOffsetX(float staticX) {
-		this.staticX = staticX;
-	}
-
-	public void setStaticOffsetY(float staticY) {
-		this.staticY = staticY;
-	}
-
-	public void setBounds(float x0, float y0, float x1, float y1) {
-		bx0 = x0;
-		by0 = y0;
-		bx1 = x1;
-		by1 = y1;
-
-		fixBounds();
-	}
-
-	public void addScreenShake(int amount) {
-		if (amount > 0) {
-			this.shakeAmount += amount;
+	private void checkBoundsX() {
+		if (pose.xOffset + this.xMove < pose.bx0) {
+			this.xMove = pose.bx0 - pose.xOffset;
+		} else if (pose.xOffset + this.xMove + viewWidth > pose.bx1) {
+			this.xMove = pose.bx1 - viewWidth - pose.xOffset;
 		}
 	}
 
-	private void finishZoom() {
-		this.zoom += zoomChange;
-		zoomChange = 0;
+	public void moveYOffset(float yMove) {
+		// The view has already been
+		// centered around the center
+		// of the bounding box.
+		if (viewHeight > pose.by1 - pose.by0)
+			return;
 		
-		if (Float.isNaN(zoom))
-			zoom = (minZoom + maxZoom) / 2.0f;
-	}
-	
-	public void setZoomInstant(float zoom) {
-		setZoom(zoom);
+		this.yMove += yMove;
 		
-		finishZoom();
+		checkBoundsY();
 	}
 
+	private void checkBoundsY() {
+		if (pose.yOffset + this.yMove < pose.by0) {
+			this.yMove = pose.by0 - pose.yOffset;
+		} else if (pose.yOffset + this.yMove + viewHeight > pose.by1) {
+			this.yMove = pose.by1 - viewHeight - pose.yOffset;
+		}
+	}
+	
+	public void moveOffset(float xMove, float yMove) {
+		moveXOffset(xMove);
+		moveYOffset(yMove);
+	}
+	
+	public void setPose(CameraPose pose, float smoothFactor) {
+		if (pose.screenSizeSet)
+			setScreenSize(pose.screenWidth, pose.screenHeight);
+		if (pose.boundsSet)
+			setBounds(pose.bx0, pose.by0, pose.bx1, pose.by1);
+		
+		smoothingPose = pose;
+		poseSmoothingFactor = smoothFactor;
+		poseSmoothingTimer = 0.0f;
+		
+		animatePose();
+	}
+	
+	public CameraPose getCameraPose() {
+		return pose.copyPose();
+	}
+	
+	public void setScreenSize(float screenWidth, float screenHeight) {
+		pose.screenWidth = screenWidth;
+		pose.screenHeight = screenHeight;
+		
+		updateViewport();
+	}
+	
+	protected void updateViewport() {
+		float s = (pose.zoom + deltaZoom) * 0.01f;
+		
+		viewWidth = pose.screenWidth / s;
+		viewHeight = pose.screenHeight / s;
+		
+		updateBounds();
+	}
+	
+	public void setScale(float scale) {
+		setZoom(scale * 100.0f);
+	}
+	
 	public void setZoom(float zoom) {
-		addZoom(zoom - this.zoom - this.zoomChange);
-	}
-
-	public void addZoom(float zoom) {
-		float xc = getCenterX(false);
-		float yc = getCenterY(false);
-
-		zoomChange += zoom;
-
-		if (this.zoom + zoomChange > maxZoom) {
-			zoomChange = maxZoom - this.zoom;
-		} else if (this.zoom + zoomChange < minZoom) {
-			zoomChange = minZoom - this.zoom;
+		if (zoom > maxZoom) {
+			deltaZoom = maxZoom - pose.zoom;
+		} else if (zoom < minZoom) {
+			deltaZoom = minZoom - pose.zoom;
+		} else {
+			deltaZoom = zoom - pose.zoom;
 		}
 		
-		if (zoomChange == 0.0f) return;
-
-		viewWidth = width / getScale();
-		viewHeight = height / getScale();
-
 		if (zoomToCenter) {
-			moveToCenter(xc, yc);
-		} else {
-			fixBounds();
+			float ns = (pose.zoom + deltaZoom) * 0.01f;
+			float nw = pose.screenWidth / ns;
+			float nh = pose.screenHeight / ns;
+			
+			xMove += (viewWidth - nw) * 0.5f;
+			yMove += (viewHeight - nh) * 0.5f;
 		}
+	
+		updateViewport();
 	}
 	
-	public void setViewport(float x0, float y0, float x1, float y1) {
-		moveTo(x0, y0);
+	public void addZoom(float deltaZoom) {
+		setZoom(pose.zoom + this.deltaZoom + deltaZoom);
+	}
+	
+	public void setMaxZoom(float maxZoom) {
+		this.maxZoom = maxZoom;
 
-		if (x0 > x1 || y0 > y1) {
+		if (maxZoom < pose.zoom + deltaZoom)
 			setZoom(maxZoom);
-		} else if (viewWidth <= 0.0f || viewHeight <= 0.0f) {
-			setZoom(minZoom);
-		} else {
-			x0 = xoffs;
-			y0 = yoffs;
-			
-			float zx = viewWidth / (x1 - x0);
-			float zy = viewHeight / (y1 - y0);
-			
-			setZoom(zx < zy ? zx : zy);
-		}
-	}
-
-	public void setViewportInstant(float x0, float y0, float x1, float y1) {
-		setViewport(x0, y0, x1, y1);
-		
-		finishMovement();
-		finishZoom();
-	}
-	
-	public float getZoom() {
-		return getZoom(1.0f);
-	}
-
-	public float getZoom(float dt) {
-		return zoom + zoomChange * dt;
-	}
-
-	public float getZoomChangeThisUpdate() {
-		return zoomChange;
-	}
-
-	public boolean zoomedThisUpdate() {
-		return zoomChange != 0.0f;
-	}
-
-	public boolean movedThisUpdate() {
-		return xmove != 0.0f || ymove != 0.0f;
-	}
-
-	public float getScale() {
-		return getScale(1.0f);
-	}
-
-	public float getScale(float dt) {
-		return getZoom(dt) / 100.0f;
 	}
 
 	public void setMinZoom(float minZoom) {
 		this.minZoom = minZoom;
+		
+		if (minZoom > pose.zoom + deltaZoom)
+			setZoom(minZoom);
 	}
-
-	public float getMinZoom() {
-		return minZoom;
-	}
-
-	public void setMaxZoom(float maxZoom) {
-		this.maxZoom = maxZoom;
-	}
-
-	public float getMaxZoom() {
-		return maxZoom;
-	}
-
+	
 	public void setZoomToCenter(boolean zoomToCenter) {
 		this.zoomToCenter = zoomToCenter;
 	}
-
-	public boolean isZoomToCenter() {
-		return zoomToCenter;
+	
+	public void setBounds(float bx0, float by0, float bx1, float by1) {
+		pose.bx0 = bx0;
+		pose.by0 = by0;
+		pose.bx1 = bx1;
+		pose.by1 = by1;
+		
+		updateBounds();
 	}
 	
-	public void setWidth(float width) {
-		this.width = width;
-		this.viewWidth = width / getScale();
+	protected void updateBounds() {
+		float bw = pose.bx1 - pose.bx0;
+		float bh = pose.by1 - pose.by0;
+		
+		if (viewWidth > bw) {
+			xMove = pose.bx0 + (bw - viewWidth) * 0.5f - pose.xOffset;
+		} else {
+			checkBoundsX();
+		}
+		if (viewHeight > bh) {
+			yMove = pose.by0 + (bh - viewHeight) * 0.5f - pose.yOffset;
+		} else {
+			checkBoundsY();
+		}
+	}
+	
+	@Override
+	public float getXOffset() {
+		return pose.xOffset;
+	}
+	
+	public float getCenterX() {
+		return pose.xOffset + viewWidth * 0.5f;
 	}
 
-	public float getWidth() {
-		return width;
+	@Override
+	public float getYOffset() {
+		return pose.yOffset;
+	}
+
+	public float getCenterY() {
+		return pose.yOffset + viewHeight * 0.5f;
+	}
+	
+	@Override
+	public Vec2f getOffset() {
+		return new Vec2f(getXOffset(), getYOffset());
+	}
+	
+	public Vec2f getCenter() {
+		return new Vec2f(getCenterX(), getCenterY());
+	}
+
+	public float getXOffset(float dt) {
+		return pose.xOffset + xMove * dt;
+	}
+
+	public float getCenterX(float dt) {
+		return getXOffset(dt) + viewWidth * 0.5f;
+	}
+	
+	public float getYOffset(float dt) {
+		return pose.yOffset + yMove * dt;
+	}
+
+	public float getCenterY(float dt) {
+		return getYOffset(dt) + viewHeight * 0.5f;
+	}
+
+	public Vec2f getOffset(float dt) {
+		return new Vec2f(getXOffset(dt), getYOffset(dt));
+	}
+
+	public Vec2f getCenter(float dt) {
+		return new Vec2f(getCenterX(dt), getCenterY(dt));
+	}
+
+	@Override
+	public float getZoom() {
+		return pose.zoom;
+	}
+
+	@Override
+	public float getScale() {
+		return pose.zoom * 0.01f;
+	}
+	
+	public float getScale(float dt) {
+		return (pose.zoom + deltaZoom * dt) * 0.01f;
+	}
+	
+	public float getScreenWidth() {
+		return pose.screenWidth;
+	}
+	
+	public float getScreenHeight() {
+		return pose.screenHeight;
 	}
 	
 	public float getViewWidth() {
 		return viewWidth;
 	}
 	
-	public void setHeight(float height) {
-		this.height = height;
-		this.viewHeight = height / getScale();
-	}
-
-	public float getHeight() {
-		return height;
-	}
-	
 	public float getViewHeight() {
 		return viewHeight;
+	}
+	
+	public static final class CameraPose {
+		
+		private float xOffset;
+		private float yOffset;
+		private float zoom;
+
+		private float screenWidth;
+		private float screenHeight;
+
+		private float bx0;
+		private float by0;
+		private float bx1;
+		private float by1;
+		
+		private boolean offsetSet;
+		private boolean zoomSet;
+		private boolean screenSizeSet;
+		private boolean boundsSet;
+		
+		public CameraPose(float xOffset, float yOffset) {
+			this();
+			
+			setOffset(xOffset, yOffset);
+		}
+
+		public CameraPose(float xOffset, float yOffset, float zoom) {
+			this();
+			
+			setOffset(xOffset, yOffset);
+			setZoom(zoom);
+		}
+
+		public CameraPose(float xOffset, float yOffset, float zoom, float screenWidth, float screenHeight) {
+			this();
+			
+			setOffset(xOffset, yOffset);
+			setZoom(zoom);
+			setScreenSize(screenWidth, screenHeight);
+		}
+		
+		private CameraPose() {
+			xOffset = 0.0f;
+			yOffset = 0.0f;
+			zoom = 100.0f;
+
+			screenWidth = 0.0f;
+			screenHeight = 0.0f;
+
+			bx0 = Float.NEGATIVE_INFINITY;
+			by0 = Float.NEGATIVE_INFINITY;
+			bx1 = Float.POSITIVE_INFINITY;
+			by1 = Float.POSITIVE_INFINITY;
+		
+			offsetSet = false;
+			zoomSet = false;
+			screenSizeSet = false;
+			boundsSet = false;
+		}
+		
+		public void setOffset(float xOffset, float yOffset) {
+			this.xOffset = xOffset;
+			this.yOffset = yOffset;
+			
+			offsetSet = true;
+		}
+
+		public void setZoom(float zoom) {
+			this.zoom = zoom;
+			
+			zoomSet = true;
+		}
+		
+		public void setScreenSize(float screenWidth, float screenHeight) {
+			this.screenWidth = screenWidth;
+			this.screenHeight = screenHeight;
+			
+			screenSizeSet = true;
+		}
+		
+		public void setBounds(float bx0, float by0, float bx1, float by1) {
+			this.bx0 = bx0;
+			this.by0 = by0;
+			this.bx1 = bx1;
+			this.by1 = by1;
+			
+			boundsSet = true;
+		}
+		
+		public CameraPose copyPose() {
+			CameraPose pose = new CameraPose();
+			if (offsetSet)
+				pose.setOffset(xOffset, yOffset);
+			if (zoomSet)
+				pose.setZoom(zoom);
+			if (screenSizeSet)
+				pose.setScreenSize(screenWidth, screenHeight);
+			if (boundsSet)
+				pose.setBounds(bx0, by0, bx1, by1);
+			return pose;
+		}
 	}
 }
